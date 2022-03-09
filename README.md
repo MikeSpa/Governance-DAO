5465a759c54551cf0436f68be2209293913c87c3
Spahr Michael <spahr.michael@protonmail.com>
2022-03-09 17:52:17 +0100

    add delegate fct to deploy-governor-token script

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 - Governance token based on OpenZeppelin ERC20Votes
 - GovernorContract made with OpenZeppelin wizard
+
+
--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -1,7 +1,9 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { ethers } from "hardhat"
 
 const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    // @ts-ignore
     const { getNamedAccounts, deployments, network } = hre //hardhat.config.ts
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts() //grab deployer account from config
@@ -13,6 +15,16 @@ const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntim
         //wait confirmations
     })
     log(`GovernanceToken deployed at ${governanceToken.address}`)
+
+    await delegate(governanceToken.address, deployer)
+    log("Delegated!")
+}
+
+const delegate = async (governanceTokenAddress: string, delegatedAccount: string) => {
+    const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddress)
+    const tx = await governanceToken.delegate(delegatedAccount)
+    await tx.wait(1)
+    console.log(`Checkpoints: ${await governanceToken.numCheckpoints(delegatedAccount)}`)
 }
 
 export default deployGovernanceToken
