262f511c009b42a8ac6f0cdd20a46bcfd33d7bc0
Spahr Michael <spahr.michael@protonmail.com>
2022-03-10 14:32:18 +0100

    add deploy script for GovernorContract

---
+++ deploy/03-deploy-governor-contract.ts
@@ -0,0 +1,32 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import {
+    QUORUM_PERCENTAGE,
+    VOTING_PERIOD,
+    VOTING_DELAY,
+} from "../helper-hardhat-config"
+
+const deployGovernorContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    // @ts-ignore
+    const { getNamedAccounts, deployments } = hre
+    const { deploy, log, get } = deployments
+    const { deployer } = await getNamedAccounts()
+    const governanceToken = await get("GovernanceToken")
+    const timeLock = await get("TimeLock")
+
+    log("Deploying GovernorContract")
+    const governorContract = await deploy("GovernorContract", {
+        from: deployer,
+        args: [
+            governanceToken.address,
+            timeLock.address,
+            VOTING_DELAY,
+            VOTING_PERIOD,
+            QUORUM_PERCENTAGE,
+        ],
+        log: true,
+    })
+    log(`GovernorContract deployed at ${governorContract.address}`)
+}
+
+export default deployGovernorContract
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,1 +1,5 @@
-export const MIN_DELAY = 3600 // 1 hour
+export const MIN_DELAY = 3600 // second -> 1 hour
+
+export const VOTING_DELAY = 1 // block
+export const VOTING_PERIOD = 5 // block
+export const QUORUM_PERCENTAGE = 10 // %
