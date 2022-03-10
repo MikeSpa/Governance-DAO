571db1f25ad07812b492b4dfba2181036fc2e77b
Spahr Michael <spahr.michael@protonmail.com>
2022-03-10 14:54:24 +0100

    add deploy script for GovernaneContract

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -3,3 +3,5 @@ export const MIN_DELAY = 3600 // second -> 1 hour
 export const VOTING_DELAY = 1 // block
 export const VOTING_PERIOD = 5 // block
 export const QUORUM_PERCENTAGE = 10 // %
+
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
